29995148bdc60c22523c8ffe5c39e2ba91c9c2e1
Matheus <matheusmoura2110@gmail.com>
2022-10-17 18:53:29 -0300

    feat: express server

---
+++ app/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.json({ message: 'server rodando'});
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
