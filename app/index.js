99553ba7584a23c22e4f1548e281007ac05ecdc6
Matheus <matheusmoura2110@gmail.com>
2022-10-20 11:23:46 -0300

    refactor: organizing folders for better reading

--- app/index.js
+++ app/index.js
@@ -1,42 +1,21 @@
 const express = require("express");
 const app = express();
-const axios = require('axios');
+
 const port = 3000;
-const connection = require('./model/config')
+
+
+
 app.use(express.json());
+const { insertRandomNames, getFormatedlistOfNames } = require('./utils/helpers')
 
 
-
+app.get('/', async (req, res) => {
+    insertRandomNames();
+    const names = await getFormatedlistOfNames();
+    res.status(200).json( !names ? `<h1>Full Cycle Rocks!</h1>` : `<h1>Full Cycle Rocks!</h1>` + names);
+})
 
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`);
 });
-
-const getRandomNames = async () => { 
-  const { data } = await axios.get('https://randomuser.me/api/?results=1');
-    const  names = data.results.map(({ name }) => name.first);
-    const formated = names[0]
-    return formated
-}
-const insertRandomNames = async (res) => { 
-  const names = await getRandomNames();
-  const query = `INSERT INTO people (name) VALUES (?)`;
-  const [result] = await connection.execute(query, [names]);
-  console.log('entrou')
-  return result;
-}
-
-app.get("/", (req, res) => {
-  insertRandomNames(res);
-  res.json({ message: `Full Cycle Rocks!`});
-});
-
-const getListOfNames = async () => { 
-  const query = `SELECT * FROM people`;
-  const [result] = await connection.execute(query);
-  console.log(result)
-  return result;
-}
-
-getListOfNames();
---
+++ app/utils/helpers.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const connection = require('../model/config')
+
+const getRandomNames = async () => { 
+    const { data } = await axios.get('https://randomuser.me/api/?results=1');
+    const  names = data.results.map(({ name }) => name.first);
+    const formated = names[0]
+    return formated
+};
+
+const insertRandomNames = async () => { 
+    const names = await getRandomNames();
+    const query = `INSERT INTO people (name) VALUES (?)`;
+    const [result] = await connection.execute(query, [names]);
+    console.log('entrou')
+    return result;
+};
+
+const getListOfNames = async () => { 
+    const query = `SELECT * FROM people`;
+    const [result] = await connection.execute(query);
+    console.log(result)
+    return result;
+};
+
+const getFormatedlistOfNames = async () => {
+    const names = await getListOfNames();
+    const formated = names.map(({ name }) => name);
+    return formated;
+}
+
+module.exports = {
+    insertRandomNames,
+    getListOfNames,
+    getFormatedlistOfNames
+};
