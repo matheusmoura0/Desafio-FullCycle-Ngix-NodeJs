5b4e4b480cb3ba62d135f3675262259d89ecc9d1
Matheus <matheusmoura2110@gmail.com>
2022-10-25 19:59:23 -0300

    att dockerfile and fix path problems

--- app/index.js
+++ app/index.js
@@ -10,12 +10,12 @@ const { insertRandomNames, getFormatedlistOfNames } = require('./utils/helpers')
 
 
 app.get('/', async (req, res) => {
-    insertRandomNames();
-    const names = await getFormatedlistOfNames();
-    res.status(200).json( !names ? `<h1>Full Cycle Rocks!</h1>` : `<h1>Full Cycle Rocks!</h1>` + names);
+  insertRandomNames();
+  const names = await getFormatedlistOfNames();
+  res.status(200).json( !names ? `<h1>Full Cycle Rocks!</h1>` : `<h1>Full Cycle Rocks!</h1>` + `<h2>Names:</h2>` + names.map((name) => `<p>${name}</p>`).join(''));
 })
 
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`);
+console.log(`Servidor rodando na porta: ${port}`);
 });
