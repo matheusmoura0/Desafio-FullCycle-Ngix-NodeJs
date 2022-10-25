be8977e8ffb44d913299722692e3ab860f83585e
Matheus <matheusmoura2110@gmail.com>
2022-10-25 20:27:37 -0300

    finished

--- app/index.js
+++ app/index.js
@@ -33,7 +33,7 @@ const { insertRandomNames, getFormatedlistOfNames } = require('./utils/helpers')
 app.get('/', async (req, res) => {
   insertRandomNames();
   const names = await getFormatedlistOfNames();
-  res.status(200).json( !names ? `<h1>Full Cycle Rocks!</h1>` : `<h1>Full Cycle Rocks!</h1>` + `<h2>Names:</h2>` + names.map((name) => `<p>${name}</p>`).join(''));
+  res.status(200).json( !names ? `Full Cycle Rocks!` : `Full Cycle Rocks!  ` + `Names:  ` + names.map((name) => `${name  }`));
 })
 
 
